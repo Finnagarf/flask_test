fb04d53f02c963cd4557a3b7db6d5bf9262bf6da
Levi W <71601642+Finnagarf@users.noreply.github.com>
2025-03-10 08:37:54 -0500

    Update hello.py

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -30,7 +30,16 @@ def results():
    color = request.form['color']
    luck_num = request.form['luck_num']
    fav_class = request.form['fav_class']
-   best_pix = request.form['best_pix']
+   best_pix = request.form['best_pix'].lower().strip()
+   films = ["toy story","a bug's life","toy story 2","monsters, inc.",
+      "finding nemo", "the incredibles","cars","ratatouille","wall-e","up",
+      "toy story 3","cars 2", "brave","monsters university","inside out",
+      "the good dinosaur","finding dory", "cars 3","coco","incredibles 2",
+      "toy story 4","onward","soul"]
+   if best_pix not in films:
+      best_pix = "Sorry, '{0}' isn't a Pixar film.".format(best_pix.title())
+   else:
+      best_pix = best_pix.title()
 
    return render_template('form_results.html', color = color, luck_num = luck_num, fav_class = fav_class, best_pix = best_pix)
 
